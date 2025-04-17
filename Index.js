b2108d7bdad8a31d73a81f89c15cf7ddca55b77f
guptajii30 <ganeshgupta10043@gmail.com>
2025-04-18 00:53:19 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -2,20 +2,9 @@ const http = require('http');
 
 const port = 3000;
 const server = http.createServer((req, res) => {
-res.end('~˛.‘˙.ª~·3˙  Welcome to the NodeJS app deployed by Ansible Pull!');
+  res.end('Welcome to the NodeJS app deployed by Ansible Pull!');
 });
 
 server.listen(port, () => {
-console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
-
-package.json
-{
-"name": "hello-node",
-"version": "1.0.0",
-"main": "index.js", "scripts": {
-"start": "node index.js"
-
-},
-"dependencies": {}
-}
